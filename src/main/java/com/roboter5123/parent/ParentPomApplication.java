79042a69aaea23fdf265aaa79aa6ae17ddae4de6
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-03-07 14:56:30 +0100

    Add files via upload

---
+++ src/main/java/com/roboter5123/parent/ParentPomApplication.java
@@ -0,0 +1,13 @@
+package com.roboter5123.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentPomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentPomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roboter5123/parent/ParentPomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roboter5123.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentPomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
