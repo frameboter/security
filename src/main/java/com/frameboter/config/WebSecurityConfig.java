7477640ba3ca5e608a814a92a8974655e1d97b7d
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-03-11 13:30:36 +0100

    Update WebSecurityConfig.java

--- src/main/java/com/frameboter/config/WebSecurityConfig.java
+++ src/main/java/com/frameboter/config/WebSecurityConfig.java
@@ -1,21 +1,27 @@
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
-/**
- * Security configuration is defined with application properties thanks to the following starters:
- * <ul>
- * <li><a href="https://github.com/ch4mpy/spring-addons/tree/master/webmvc/spring-addons-webmvc-client">spring-addons-webmvc-jwt-client</a> which defines a
- * "client" security filter-chain (sessions, CSRF protection, OAuth2 login &amp; logout, redirect unauthorized requests to login with 302) with high &#64;Order
- * and security-matchers defined with com.c4-soft.springaddons.oidc.client.security-matchers property</li>
- * <li><a href=
- * "https://github.com/ch4mpy/spring-addons/tree/master/webmvc/spring-addons-webmvc-jwt-resource-server">spring-addons-webmvc-jwt-resource-server</a> which
- * defines a "resource server" security filter-chain (no session, no CSRF protection, no login nor logout, returns 401 for unauthorized requests) with lowest
- * &#64;Order and no security-matchers (processes all requests that were not processed by other security filter-chains)</li>
- * </ul>
- *
- * @author Jerome Wacongne ch4mp&#64;c4-soft.com
- */
+@RequiredArgsConstructor
 @Configuration
-@EnableMethodSecurity
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+  private final JwtAuthConverter jwtAuthConverter;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+      http.cors().and().csrf().disable();
+      http.authorizeHttpRequests()
+              .anyRequest().authenticated();
+      http.oauth2ResourceServer()
+              .jwt()
+              .jwtAuthenticationConverter(jwtAuthConverter);
+      http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+      return http.build();
+  }
 }
