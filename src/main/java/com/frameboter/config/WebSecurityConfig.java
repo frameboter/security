e6da8792676295c053ec072f9c85bfe2907f025b
roboter5123 <pascal.roboter@hotmail.com>
2024-03-12 19:11:57 +0100

    Fixed auto configuration

--- src/main/java/com/frameboter/config/WebSecurityConfig.java
+++ src/main/java/com/frameboter/config/WebSecurityConfig.java
@@ -1,31 +1,26 @@
 package com.frameboter.config;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
-@AutoConfiguration
+@Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(jsr250Enabled = true)
 public class WebSecurityConfig {
 
-  private final JwtAuthConverter jwtAuthConverter = new JwtAuthConverter();
+    private final JwtAuthConverter jwtAuthConverter = new JwtAuthConverter();
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-      http.cors().and().csrf().disable();
-      http.authorizeHttpRequests()
-              .anyRequest().authenticated();
-      http.oauth2ResourceServer()
-              .jwt()
-              .jwtAuthenticationConverter(jwtAuthConverter);
-      http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-      return http.build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable();
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthConverter);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
 }
