ef985f5ee5c5c0a9eb8c1b24f2d8322ed83ea7a1
roboter5123 <pascal.roboter@hotmail.com>
2024-03-12 18:42:13 +0100

    Revert "Fixed jwt conversion actually this time"
    This reverts commit 9fadca00e8f6cfafacf0f190d3c263cf0d76dffa.

--- src/main/java/com/frameboter/config/JwtAuthConverter.java
+++ src/main/java/com/frameboter/config/JwtAuthConverter.java
@@ -18,7 +18,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@Component
+@Service
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
   private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter;
--- src/main/java/com/frameboter/config/WebSecurityConfig.java
+++ src/main/java/com/frameboter/config/WebSecurityConfig.java
@@ -1,30 +1,31 @@
 package com.frameboter.config;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@RequiredArgsConstructor
 @AutoConfiguration
 @EnableWebSecurity
 @EnableMethodSecurity(jsr250Enabled = true)
 public class WebSecurityConfig {
 
-    private final JwtAuthConverter jwtAuthConverter;
+  private final JwtAuthConverter jwtAuthConverter;
 
-    public WebSecurityConfig() {
-        this.jwtAuthConverter = new JwtAuthConverter();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.cors().and().csrf().disable();
-        http.authorizeHttpRequests().anyRequest().authenticated();
-        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthConverter);
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        return http.build();
-    }
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+      http.cors().and().csrf().disable();
+      http.authorizeHttpRequests()
+              .anyRequest().authenticated();
+      http.oauth2ResourceServer()
+              .jwt()
+              .jwtAuthenticationConverter(jwtAuthConverter);
+      http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+      return http.build();
+  }
 }
