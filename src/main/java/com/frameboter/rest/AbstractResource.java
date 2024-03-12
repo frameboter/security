0ba62938c8ec590966cd77a749fe97d265781a97
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-03-12 09:51:15 +0100

    Update AbstractResource.java

--- src/main/java/com/frameboter/rest/AbstractResource.java
+++ src/main/java/com/frameboter/rest/AbstractResource.java
@@ -6,8 +6,13 @@ import org.springframework.security.oauth2.jwt.Jwt;
 public class AbstractResource {
 
   // @AuthenticationPrincipal 
+  protected final Object getUserId(Jwt jwt){
+    return jwt.getSubject();
+  }
+
+    // @AuthenticationPrincipal 
   protected final Object getUserName(Jwt jwt){
-    return jwt.getClaimAsString("upn");
+    return jwt.getClaimAsString("preferred_username");
   }
 
   protected final Object getRoles(Jwt jwt){
