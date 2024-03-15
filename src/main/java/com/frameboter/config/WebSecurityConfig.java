5e7f25dbe39168c4a966ad14c2ec851f435d002d
roboter5123 <pascal.roboter@hotmail.com>
2024-03-15 14:14:39 +0100

    Updated config dor swagger ui

--- src/main/java/com/frameboter/config/WebSecurityConfig.java
+++ src/main/java/com/frameboter/config/WebSecurityConfig.java
@@ -31,8 +31,8 @@ public class WebSecurityConfig {
 
         http.cors().and().csrf().disable();
         http.authorizeHttpRequests()
-                .requestMatchers(swaggerPath + "/*").permitAll()
-                .requestMatchers(swaggerPath + "-ui/*").permitAll()
+                .requestMatchers(swaggerPath).permitAll()
+                .requestMatchers(swaggerPath + "-ui").permitAll()
                 .requestMatchers(openApiPath + "/*").permitAll()
                 .anyRequest().authenticated();
         http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthConverter);
