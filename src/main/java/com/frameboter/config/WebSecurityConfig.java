0645d01461d23bdfab868d966ef9ad92624cef10
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-03-19 10:44:59 +0100

    Update WebSecurityConfig.java

--- src/main/java/com/frameboter/config/WebSecurityConfig.java
+++ src/main/java/com/frameboter/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.frameboter.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
